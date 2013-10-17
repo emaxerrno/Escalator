8dbf9f0e655d1da0a500f59daf927878ff06c928
Alex Wilson <alex.wilson@navetas.com>
2013-10-17 15:51:50 +0100

    Move to latest sbt-cpp with sbt 0.13 upgrade.

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 set -e
 
 rm -rf target
-./sbt "native-build-configuration Gcc_LinuxPC_Release" test
+./sbt "nativeBuildConfiguration Gcc_LinuxPC_Release" test
 #./sbt "native-build-configuration Clang_LinuxPC_Release" test
